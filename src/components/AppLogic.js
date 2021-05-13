cde2ae58dcc9892df59b471fd1f1c1abeb7bc5a2
Sholto <madein.mania@gmail.com>
2021-05-13 09:41:33 +0100

    check if clicked, and add to clicked array methods

--- src/components/AppLogic.js
+++ src/components/AppLogic.js
@@ -1,9 +1,18 @@
 export const AppLogic = (() => {
+  const checkIfClicked = (clicked, clickedArray) => {
+    if (clickedArray.includes(clicked)) return true;
+    return false;
+  };
+  const addToClickedArray = (clicked, clickedArray) => {
+    return clickedArray.concat(clicked);
+  };
   const shuffle = (array) => {
     return array.sort(() => Math.random() - 0.5);
   };
 
   return {
+    checkIfClicked,
+    addToClickedArray,
     shuffle,
   };
 })();
