3c318e8420b60095fb29614f5214b59143981916
Sholto <madein.mania@gmail.com>
2021-05-13 15:15:34 +0100

    scoreboard display component

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Scoreboard = (props) => {
-  return <p>Scoreboard works!</p>;
+  return (
+    <div>
+      <p>Score: {props.score}</p>
+      <p>Highscore: {props.highScore}</p>
+    </div>
+  );
 };
 
 export default Scoreboard;
