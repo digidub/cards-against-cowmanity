4f1f68f628734e1f014550ea63be690656d37998
Sholto <madein.mania@gmail.com>
2021-05-13 10:38:24 +0100

    prototyping logic for clicking and adding to clicked list

--- src/components/GridContainer.jsx
+++ src/components/GridContainer.jsx
@@ -1,15 +1,32 @@
 import React, { useEffect, useState } from 'react';
+import { AppLogic } from './AppLogic';
 
 const GridContainer = (props) => {
   const [state, setState] = useState(['cow1', 'cow2', 'cow3']);
+  const [clicked, setClicked] = useState([]);
+
+  const handleClick = (e) => {
+    const clickedCow = e.target.innerText;
+    console.log(clickedCow);
+    if (AppLogic.checkIfClicked(clickedCow, clicked)) console.log('YOU LOSE!!!');
+    else setClicked(clicked.concat(clickedCow));
+    const randomised = AppLogic.shuffle(state);
+    setState(randomised);
+  };
 
   const cowCards = state.map((cow) => {
-    return <li>{cow}</li>;
+    return (
+      <li key={cow} onClick={handleClick}>
+        {cow}
+      </li>
+    );
   });
 
-  useEffect(() => {}, []);
-
-  return <ul>{cowCards}</ul>;
+  return (
+    <div>
+      <ul>{cowCards}</ul>
+    </div>
+  );
 };
 
 export default GridContainer;
