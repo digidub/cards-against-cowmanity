7381b80c2d66242d3dc573b48fca0c6262fe2eca
Sholto <madein.mania@gmail.com>
2021-05-13 15:16:02 +0100

    moved logic out of GridContainer

--- src/components/GridContainer.jsx
+++ src/components/GridContainer.jsx
@@ -1,32 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { AppLogic } from './AppLogic';
+import React from 'react';
+import Card from './Card';
+import '../assets/stylesheets/GridContainer.css';
 
 const GridContainer = (props) => {
-  const [state, setState] = useState(['cow1', 'cow2', 'cow3']);
-  const [clicked, setClicked] = useState([]);
-
-  const handleClick = (e) => {
-    const clickedCow = e.target.innerText;
-    console.log(clickedCow);
-    if (AppLogic.checkIfClicked(clickedCow, clicked)) console.log('YOU LOSE!!!');
-    else setClicked(clicked.concat(clickedCow));
-    const randomised = AppLogic.shuffle(state);
-    setState(randomised);
-  };
-
-  const cowCards = state.map((cow) => {
+  const cowCards = props.cows.map((cow) => {
     return (
-      <li key={cow} onClick={handleClick}>
-        {cow}
-      </li>
+      <div className='grid-container' key={cow}>
+        <Card image={cow} clickFunction={props.clickFunction} />
+      </div>
     );
   });
 
-  return (
-    <div>
-      <ul>{cowCards}</ul>
-    </div>
-  );
+  return <div className='game-grid'>{cowCards}</div>;
 };
 
 export default GridContainer;
