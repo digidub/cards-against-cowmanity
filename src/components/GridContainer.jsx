d48a4c77ba8e81c3b413224d310667910237c8c7
Sholto <madein.mania@gmail.com>
2021-05-13 09:53:41 +0100

    generate example data for testing

--- src/components/GridContainer.jsx
+++ src/components/GridContainer.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const GridContainer = (props) => {
-  return <p>test</p>;
+  const [state, setState] = useState(['cow1', 'cow2', 'cow3']);
+
+  const cowCards = state.map((cow) => {
+    return <li>{cow}</li>;
+  });
+
+  useEffect(() => {}, []);
+
+  return <ul>{cowCards}</ul>;
 };
 
 export default GridContainer;
+
+// const GameArray = () => {
+//   const [state, setState] = useState(['cow1', 'cow2', 'cow3']);
+
+//   const cowCards = state.map((cow) => {
+//     <li>{cow}</li>;
+//   });
+
+//   useEffect(() => {}, []);
+
+//   return <ul>{cowCards}</ul>;
+// };
