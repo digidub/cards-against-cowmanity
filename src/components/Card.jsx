f406e5cb08e36d4d9f03161404855ada23cecd5f
Sholto <madein.mania@gmail.com>
2021-05-13 15:11:02 +0100

    import images and display them

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
 
-const Card = (props) => {};
+function importAll(r) {
+  let images = {};
+  r.keys().map((item, index) => {
+    images[item.replace('./', '')] = r(item);
+  });
+  return images;
+}
+
+const images = importAll(require.context('../assets', false, /\.(png|jpe?g|svg)$/));
+console.log(images);
+
+const Card = (props) => {
+  return <img src={images[props.image].default} alt='cow' />;
+};
 
 export default Card;
