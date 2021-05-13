1d3ff8b7471fd65b64ddb836a5b32a097a40f048
Sholto <madein.mania@gmail.com>
2021-05-13 15:13:40 +0100

    added array of images

--- src/App.js
+++ src/App.js
@@ -5,7 +5,24 @@ import { AppLogic } from './components/AppLogic';
 import Scoreboard from './components/Scoreboard';
 
 const App = (props) => {
-  const [cows, setCows] = useState(['cow1', 'cow2', 'cow3']);
+  const [cows, setCows] = useState([
+    '1.jpg',
+    '2.jpg',
+    '3.jpg',
+    '4.jpg',
+    '5.jpg',
+    '6.jpg',
+    '7.jpg',
+    '8.jpg',
+    '9.jpg',
+    '10.jpg',
+    '11.jpg',
+    '12.jpg',
+    '13.jpg',
+    '14.jpg',
+    '15.jpg',
+    '16.jpg',
+  ]);
   const [clicked, setClicked] = useState([]);
   const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
