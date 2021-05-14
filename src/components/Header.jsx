94d2e87ab07495005f9c2cc23b8e83c417603b21
Sholto <madein.mania@gmail.com>
2021-05-14 14:57:32 +0100

    added header text, instructions

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import '../assets/stylesheets/Header.css';
 
 const Header = () => {
-  return <h1>COWDS AGAINST COWMANITY</h1>;
+  return (
+    <Fragment>
+      <h1>🐄 COWDS AGAINST COWMANITY 🐄</h1>
+      <p>Challenge your memoory in this tantalising cow themed game suitable for all ages.</p>
+      <p>Click all 16 cows to achieve total cowtopia!!! But beware, clicking the same cow twice will reset your score to zero. 😈</p>
+    </Fragment>
+  );
 };
 
 export default Header;
