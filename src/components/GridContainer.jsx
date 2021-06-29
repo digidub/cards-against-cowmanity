cdf43f5380e31518ad9239590d249156d90c9c3c
Sholto <madein.mania@gmail.com>
2021-06-29 08:22:22 +0100

    removed old code comments

--- src/components/GridContainer.jsx
+++ src/components/GridContainer.jsx
@@ -15,15 +15,3 @@ const GridContainer = (props) => {
 };
 
 export default GridContainer;
-
-// const GameArray = () => {
-//   const [state, setState] = useState(['cow1', 'cow2', 'cow3']);
-
-//   const cowCards = state.map((cow) => {
-//     <li>{cow}</li>;
-//   });
-
-//   useEffect(() => {}, []);
-
-//   return <ul>{cowCards}</ul>;
-// };
